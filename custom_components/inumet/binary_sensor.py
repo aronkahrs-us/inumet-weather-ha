f8ad1645adbaf58cef627c74658f8058c82f2573
Aron Kahrs <aronkahrs.us@gmail.com>
2024-01-30 22:07:59 +0000

    fixed attribution

--- custom_components/inumet/binary_sensor.py
+++ custom_components/inumet/binary_sensor.py
@@ -9,6 +9,7 @@ from homeassistant.components.binary_sensor import (
 from .const import DOMAIN
 from .coordinator import InumetDataUpdateCoordinator
 from .entity import InumetEntity
+import datetime as dt
 
 ENTITY_DESCRIPTIONS = (
     BinarySensorEntityDescription(
@@ -47,6 +48,7 @@ class InumetBinarySensor(InumetEntity, BinarySensorEntity):
         """Initialize the binary_sensor class."""
         super().__init__(coordinator)
         self.entity_description = entity_description
+        self._attr_attribution = "Data provided by INUMET"
         self._attr_unique_id = f"{DOMAIN}_{self.entity_description.key}_{self.coordinator.data['estado'].get('id')}"
 
     @property
@@ -57,7 +59,9 @@ class InumetBinarySensor(InumetEntity, BinarySensorEntity):
             _colores = ['','Amarilla','Naranja','Roja']
             riesgos = [x['riesgoFenomeno'] for x in alerts['advertencias']][0]
             descripcion = [x['descripcion'] for x in alerts['advertencias']][0]
+            fechas = [{'inicio':dt.datetime.strptime(x['comienzo'],'%Y-%m-%d'),'fin':(dt.datetime.strptime(x['finalizacion'],'%Y-%m-%d')+ dt.timedelta(days=1))} for x in alerts['advertencias']][0]
             self.extra_state_attributes = {
+                    "fenomeno": [x['fenomeno'] for x in alerts['advertencias']][0],
                     "riesgoViento": riesgos["riesgoViento"],
                     "riesgoLluvia": riesgos["riesgoLluvia"],
                     "riesgoTormenta": riesgos["riesgoTormenta"],
@@ -68,6 +72,9 @@ class InumetBinarySensor(InumetEntity, BinarySensorEntity):
                     "descripcion": descripcion,
                 }
             zones = [[i['label'] for i in x['zonasArray']] for x in alerts['advertencias']][0]
-            return self.coordinator.client.depto in zones
-        except:
+            if self.coordinator.client.depto in zones and fechas['inicio'] < dt.datetime.now() < fechas['fin']:
+                return True
+            return False
+        except Exception as e:
+            print('----EXCEPTION----',e)
             return False
