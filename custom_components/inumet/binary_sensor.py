fd32d189c7b32db7a95789376f9f246866e11fa9
Aron Kahrs <aronkahrs.us@gmail.com>
2024-01-30 21:42:07 +0000

    Fixed advertencias and added avisos

--- custom_components/inumet/binary_sensor.py
+++ custom_components/inumet/binary_sensor.py
@@ -16,6 +16,11 @@ ENTITY_DESCRIPTIONS = (
         name="Alerta",
         icon="mdi:alert-circle"
     ),
+    BinarySensorEntityDescription(
+        key="avisos",
+        name="Aviso",
+        icon="mdi:information"
+    ),
 )
 
 
@@ -42,13 +47,27 @@ class InumetBinarySensor(InumetEntity, BinarySensorEntity):
         """Initialize the binary_sensor class."""
         super().__init__(coordinator)
         self.entity_description = entity_description
+        self._attr_unique_id = f"{DOMAIN}_{self.entity_description.key}_{self.coordinator.data['estado'].get('id')}"
 
     @property
     def is_on(self) -> bool:
         """Return true if the binary_sensor is on."""
         try:
-            alerts = self.coordinator.data.get('advertencias').get(self.entity_description.key)
-            zones = [x['zonasArray'] for x in alerts]
-            return self.coordinator.client.depto in [x['id'] for x in zones]
+            alerts = self.coordinator.data.get(self.entity_description.key)
+            _colores = ['','Amarilla','Naranja','Roja']
+            riesgos = [x['riesgoFenomeno'] for x in alerts['advertencias']][0]
+            descripcion = [x['descripcion'] for x in alerts['advertencias']][0]
+            self.extra_state_attributes = {
+                    "riesgoViento": riesgos["riesgoViento"],
+                    "riesgoLluvia": riesgos["riesgoLluvia"],
+                    "riesgoTormenta": riesgos["riesgoTormenta"],
+                    "riesgoVisibilidad": riesgos["riesgoVisibilidad"],
+                    "riesgoCalor": riesgos["riesgoCalor"],
+                    "riesgoFrio": riesgos["riesgoFrio"],
+                    "alertaColor": _colores[riesgos[max(riesgos, key=riesgos.get)]],
+                    "descripcion": descripcion,
+                }
+            zones = [[i['label'] for i in x['zonasArray']] for x in alerts['advertencias']][0]
+            return self.coordinator.client.depto in zones
         except:
             return False
