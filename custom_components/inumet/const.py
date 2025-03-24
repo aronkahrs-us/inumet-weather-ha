005222fd94ea349b28f98234e27cfafdb25af6a4
Aron Kahrs <aronkahrs.us@gmail.com>
2025-03-24 13:14:49 +0000

    Bump to api 0.2.2

--- custom_components/inumet/const.py
+++ custom_components/inumet/const.py
@@ -6,7 +6,7 @@ LOGGER: Logger = getLogger(__package__)
 NAME = "Inumet"
 DOMAIN = "inumet"
 MANUFACTURER: str = "Aron Kahrs"
-VERSION = "0.4.2"
+VERSION = "0.4.3"
 ATTRIBUTION = "Data provided by Inumet"
 
 UPDATE_INTERVAL = 30
