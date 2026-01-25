884b1b9107dc7aae49053317ebd1f6b47801ccb5
Aron Kahrs <aronkahrs.us@gmail.com>
2026-01-25 21:23:51 +0000

    bump versions

--- custom_components/inumet/const.py
+++ custom_components/inumet/const.py
@@ -7,7 +7,7 @@ LOGGER: Logger = getLogger(__package__)
 NAME = "Inumet"
 DOMAIN = "inumet"
 MANUFACTURER: str = "Aron Kahrs"
-VERSION = "0.5.2"
+VERSION = "0.5.3"
 ATTRIBUTION = "Data provided by Inumet"
 
 UPDATE_INTERVAL = 30
