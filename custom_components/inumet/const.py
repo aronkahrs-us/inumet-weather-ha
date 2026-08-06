ce7fb1e25a8bf57bc9b750bc1409c73f2c970ec5
Aron Kahrs <aronkahrs.us@gmail.com>
2026-08-06 14:46:12 -0300

    Bump version to 0.5.5 in const.py and manifest.json

--- custom_components/inumet/const.py
+++ custom_components/inumet/const.py
@@ -7,7 +7,7 @@ LOGGER: Logger = getLogger(__package__)
 NAME = "Inumet"
 DOMAIN = "inumet"
 MANUFACTURER: str = "Aron Kahrs"
-VERSION = "0.5.4"
+VERSION = "0.5.5"
 ATTRIBUTION = "Data provided by Inumet"
 
 UPDATE_INTERVAL = 30
